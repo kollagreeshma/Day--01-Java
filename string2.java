bed213572a818bfd0198ca55b9c937572926cad0
kollagreeshma <kollagreeshmasree2@gmail.com>
2024-12-02 16:11:56 +0530

    Add files via upload

---
+++ string2.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class string2{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        String a=sc.next();
+        String b=String.format("congratulation %s you won 5 lakhs",a);
+        System.out.println(b);
+    }
+}
